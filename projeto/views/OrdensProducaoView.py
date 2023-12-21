a16fb6159605db94142fdeb685c8c296c45db7a4
Simão Teixeira <simaotexeira15@gmail.com>
2023-12-21 15:30:41 +0000

    ...

--- projeto/views/OrdensProducaoView.py
+++ projeto/views/OrdensProducaoView.py
@@ -1,8 +1,11 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
+from projeto.forms.ProductionOrdersForm import ProductionOrdersForm
 from projeto.repositories.ProductionOrdersRepo import ProductionOrdersRepo
+from projeto.repositories.ProductsRepo import ProductsRepo
 from projeto.tables.ProductionOrdersTable import ProductionOrdersTable
+from projeto.tables.SelectTables.SelectProductsTable import SelectProductsTable
 
 
 @login_required(login_url='/login')
@@ -17,3 +20,18 @@ def home(request):
     }
 
     return render(request, 'ordensProducao/index.html', context)
+
+@login_required(login_url='/login')
+def create(request):
+    form = ProductionOrdersForm(request.POST or None)
+    equipments = ProductsRepo().find_all_products()
+
+    equipmentsTable = SelectProductsTable(equipments)
+
+    context = {
+        'form': form,
+        'equipments': equipments,
+        'selectEquipmentsTable': equipmentsTable,
+        'navSection': 'producao',
+        'navSubSection': 'ordensProducao',
+    }
